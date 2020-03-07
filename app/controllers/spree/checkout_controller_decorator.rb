24c96865cf929cf52386a6413b3617bc453f6d8b
BoomPowPow <henry.valdez.gil@gmail.com>
2020-03-07 16:11:34 -0500

    check if zeitwerk will work like this

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,23 +1,20 @@
-module SpreeReffiliate
-  module Spree
-    module CheckoutControllerDecorator
-      def self.prepended(base)
-        base.before_action :set_affilate, only: :update
-        base.after_action :clear_session, only: :update
-      end
-    
-      private
-        def set_affilate
-          if @order.payment? && session[:affiliate]
-            @order.affiliate = Spree::Affiliate.find_by(path: session[:affiliate])
-          end
-        end
-    
-        def clear_session
-          session[:affiliate] = nil if @order.completed?
-        end
+module Spree
+  module CheckoutControllerDecorator
+    def self.prepended(base)
+      base.before_action :set_affilate, only: :update
+      base.after_action :clear_session, only: :update
     end
+  
+    private
+      def set_affilate
+        if @order.payment? && session[:affiliate]
+          @order.affiliate = Spree::Affiliate.find_by(path: session[:affiliate])
+        end
+      end
+  
+      def clear_session
+        session[:affiliate] = nil if @order.completed?
+      end
   end
+  CheckoutController.prepend(CheckoutControllerDecorator)
 end
-
-Spree::CheckoutController.prepend(SpreeReffiliate::Spree::CheckoutControllerDecorator)
