d712d3a8da26e287adf371de714e2a269ef6ee12
BoomPowPow <henry.valdez.gil@gmail.com>
2020-03-07 15:07:28 -0500

    Updated controller for Spree 4.X

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,15 +1,23 @@
-Spree::CheckoutController.class_eval do
-  before_action :set_affilate, only: :update
-  after_action :clear_session, only: :update
-
-  private
-    def set_affilate
-      if @order.payment? && session[:affiliate]
-        @order.affiliate = Spree::Affiliate.find_by(path: session[:affiliate])
+module SpreeReffiliate
+  module Spree
+    module CheckoutControllerDecorator
+      def self.prepended(base)
+        base.before_action :set_affilate, only: :update
+        base.after_action :clear_session, only: :update
       end
+    
+      private
+        def set_affilate
+          if @order.payment? && session[:affiliate]
+            @order.affiliate = Spree::Affiliate.find_by(path: session[:affiliate])
+          end
+        end
+    
+        def clear_session
+          session[:affiliate] = nil if @order.completed?
+        end
     end
-
-    def clear_session
-      session[:affiliate] = nil if @order.completed?
-    end
+  end
 end
+
+::Spree::CheckoutController.prepend SpreeReffiliate::Spree::CheckoutControllerDecorator
