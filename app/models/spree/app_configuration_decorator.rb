aff4639141122f9c0e05b8c97883288cc657b0fb
BoomPowPow <henry.valdez.gil@gmail.com>
2020-03-13 00:54:56 -0400

    reverted it, seems it didn't work

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,12 +1,12 @@
 module Spree
   module AppConfigurationDecorator
     def self.prepended(base)
-      preference :admin_affiliates_per_page, :integer, default: 10
-      preference :admin_commissions_per_page, :integer, default: 10
-      preference :admin_transactions_per_page, :integer, default: 10
-      preference :referred_records_per_page, :integer, default: 10
-      preference :referral_credits, :decimal, default: 0
-      preference :referrer_benefit_enabled, :boolean, default: false
+      base.preference :admin_affiliates_per_page, :integer, default: 10
+      base.preference :admin_commissions_per_page, :integer, default: 10
+      base.preference :admin_transactions_per_page, :integer, default: 10
+      base.preference :referred_records_per_page, :integer, default: 10
+      base.preference :referral_credits, :decimal, default: 0
+      base.preference :referrer_benefit_enabled, :boolean, default: false
     end
   end
   AppConfiguration.prepend(AppConfigurationDecorator)
