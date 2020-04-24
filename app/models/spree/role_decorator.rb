6278cf747c820e53a1ea9f5350fea89c0807eef3
BoomPowPow <henry.valdez.gil@gmail.com>
2020-04-24 19:33:07 -0400

    Corrected Zeitwerk error

--- app/models/spree/role_decorator.rb
+++ app/models/spree/role_decorator.rb
@@ -1,7 +1,8 @@
-Spree::Role.class_eval do
-  class << self
+module Spree
+  module RoleDecorator
     define_method :affiliate do
       find_or_create_by(name: :affiliate)
     end
   end
+  Role.prepend(RoleDecorator)
 end
