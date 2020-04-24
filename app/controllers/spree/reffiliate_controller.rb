38b2a3139bd083c21c63b45ef9831ff15124c603
BoomPowPow <henry.valdez.gil@gmail.com>
2020-04-24 18:07:34 -0400

    Flash message correction

--- app/controllers/spree/reffiliate_controller.rb
+++ app/controllers/spree/reffiliate_controller.rb
@@ -1,17 +1,18 @@
 module Spree
   class ReffiliateController < Spree::StoreController
     def referral
-      flash[:success] = Spree.t(:referral_code_applied)
+      # flash[:success] = Spree.t(:referral_code_applied)
       session[:referral] = params[:code]
-      redirect_to root_path
+      redirect_to root_path, success: Spree.t(:referral_code_applied)
     end
 
     def affiliate
       session[:affiliate] = params[:path]
       affiliate = Spree::Affiliate.find_by(path: params[:path])
+      session[:affiliate_data] = affiliate
       flash[:success] = Spree.t(:affiliate_code_applied) unless affiliate.nil?
       if affiliate.nil? or affiliate.partial.blank? or !partial_exists affiliate.partial
-        redirect_to(root_path)
+        redirect_to root_path
       elsif partial_exists affiliate.partial
         render "spree/affiliates/#{affiliate.partial}", layout: affiliate.get_layout
       end
